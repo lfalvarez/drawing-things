7a626fe3fe3bedb55c4ca156872aae6fe1d4cfa2
Felipe Álvarez <falvarez@votainteligente.cl>
2014-08-27 20:28:39 -0400

    drawing a circle

--- js/drawer.js
+++ js/drawer.js
@@ -1,1 +1,26 @@
-var graph = new joint.dia.Graph;
+var graph;
+var paper;
+var current_status;
+$(function(){
+	graph = new joint.dia.Graph;
+	current_status = {
+		'action' : 'nothing'
+	};
+	paper = new joint.dia.Paper({
+	    el: $('#persons_graph'),
+	    width: 600,
+	    height: 200,
+	    model: graph,
+	    gridSize: 1,
+
+	});
+	paper.on('blank:pointerclick', function(evt, x, y){
+		var person = new joint.shapes.basic.Circle({
+			'position' : {
+				'x':x,
+				'y':y
+			}
+		});
+		graph.addCell(person)
+	})
+})
--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,21 @@
+QUnit.module( "Basics" );
 QUnit.test( "there is a graph element", function( assert ) {
 	assert.ok(graph);
+	assert.ok(paper)
+	assert.deepEqual(paper.model, graph)
+});
+QUnit.test("There is a current status object", function(assert){
+	assert.ok(current_status);
+	assert.deepEqual(current_status.action, "nothing");
+});
+QUnit.test('If I change the status to "add_person" and click in the graph then it adds a circle', function(assert){
+	current_status = 'add_person';
+	paper.trigger('blank:pointerclick', {}, 1, 100);
+	assert.equal(graph.getElements().length, 1)
+	var element = graph.getElements()[0];
+	var circle = element.attr('circle');
+	var position = element.get('position');
+	assert.equal(position.x, 1)
+	assert.equal(position.y, 100)
+
 });
