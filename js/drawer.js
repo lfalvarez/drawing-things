0da6f15c84d76286ac932b6f4ec591ecfc5a4b42
Felipe Álvarez <falvarez@votainteligente.cl>
2014-08-29 17:56:15 -0400

    Refactoring and other stuff

--- js/drawer.js
+++ js/drawer.js
@@ -1,9 +1,16 @@
 function PeopleMapper (el) {
-	this.graph = new joint.dia.Graph;
+	var Graph = joint.dia.Graph;
+	Graph.prototype.current_status = {
+		'action' : 'nothing'	
+	}
+	Graph.prototype.get_action = function(){
+		return this.current_status.action;
+	}
+	Graph.prototype.set_action = function(action){
+		this.current_status.action = action;
+	}
+	this.graph = new Graph;
 	this.el = el;
-	this.current_status = {
-		'action' : 'nothing'
-	};
 	this.paper = new joint.dia.Paper({
 	    el: el,
 	    width: 600,
@@ -12,14 +19,24 @@ function PeopleMapper (el) {
 	    gridSize: 1,
 
 	});
+
+	this.get_action = function(){
+		return this.graph.get_action()
+	}
+	this.set_status = function(status){
+		this.graph.set_action(status)
+	}
 	this.paper.on('blank:pointerclick', function(evt, x, y){
-		var person = new joint.shapes.basic.Circle({
-			'position' : {
-				'x':x,
-				'y':y
-			}
-		});
-		this.model.addCell(person)
+		if (this.model.get_action()=="add_person"){
+			var person = new joint.shapes.basic.Circle({
+				'position' : {
+					'x':x,
+					'y':y
+				}
+			});
+			this.model.addCell(person)
+			this.model.set_action('nothing')
+		}
 	})
 	
 }
--- tests/tests.js
+++ tests/tests.js
@@ -13,11 +13,11 @@ QUnit.test( "there is a graph element", function( assert ) {
 	assert.deepEqual(this.people_mapper.paper.model, this.people_mapper.graph)
 });
 QUnit.test("There is a current status object", function(assert){
-	assert.ok(this.people_mapper.current_status);
-	assert.deepEqual(this.people_mapper.current_status.action, "nothing");
+	assert.deepEqual(this.people_mapper.get_action(), "nothing");
 });
 QUnit.test('If I change the status to "add_person" and click in the graph then it adds a circle', function(assert){
-	this.people_mapper.current_status = 'add_person';
+	this.people_mapper.set_status('add_person');
+
 	this.people_mapper.paper.trigger('blank:pointerclick', {}, 1, 100);
 	assert.equal(this.people_mapper.graph.getElements().length, 1)
 	var element = this.people_mapper.graph.getElements()[0];
@@ -25,5 +25,28 @@ QUnit.test('If I change the status to "add_person" and click in the graph then i
 	var position = element.get('position');
 	assert.equal(position.x, 1)
 	assert.equal(position.y, 100)
-
+	assert.equal(this.people_mapper.get_action(), 'nothing');
 });
+QUnit.test('if status isnt set to add_person then it doesnt add anything', function(assert){
+	this.people_mapper.graph.clear()
+	this.people_mapper.paper.trigger('blank:pointerclick', {}, 1, 100);
+	assert.equal(this.people_mapper.graph.getElements().length, 0)
+
+})
+QUnit.module("Graph with controls", {
+	setup : function() {
+		var element_to_which_the_graph_will_be_added = $('#persons_graph_test');
+		var people_mapper = new PeopleMapper(element_to_which_the_graph_will_be_added);
+		this.graph = people_mapper.graph;
+
+	},
+	teardown: function(){
+	}
+})
+QUnit.test('graph has a current status', function(assert){
+	assert.equal(this.graph.get_action(), "nothing")
+})
+QUnit.test('set current status', function(assert){
+	this.graph.set_action('something');
+	assert.equal(this.graph.get_action(),'something')
+})
